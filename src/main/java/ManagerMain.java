c9321b47a29b12015f104a61727df964f6c3c2ab
Omar alMasadeh <omarjmasadeh@gmail.com>
2020-01-25 20:53:57 +0200

    add files 2

--- src/main/java/ManagerMain.java
+++ src/main/java/ManagerMain.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class ManagerMain {
     public static void main(String[] args) {
-        try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("MapReduce.obj"))) {
+        try(ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("/home/MapReduce/target/MapReduce.obj"))) {
             MapReduce mapReduce = (MapReduce)objectInputStream.readObject();
             ArrayList<String> arrayList = FileUtil.ReadInputFile();
             SplittingServer splittingServer = new SplittingServer(mapReduce,arrayList);
