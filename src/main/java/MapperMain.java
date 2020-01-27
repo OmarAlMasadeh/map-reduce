bbcf98849339e6e8bdcab72d5a98fbaa862829ea
Omar alMasadeh <omarjmasadeh@gmail.com>
2020-01-27 22:20:19 +0200

    Adding files

--- src/main/java/MapperMain.java
+++ src/main/java/MapperMain.java
@@ -11,13 +11,15 @@ public class MapperMain {
         ArrayList<String> Split = splitReceiver.receiveSplit();
         System.out.println("Received Split of " + Split.size() + " Words");
         System.out.println("Mapping...");
-        HashMap hashMap = mapReduce.getMapper().Map(Split);
+        HashMap<String,ArrayList<Integer>> hashMap = mapReduce.getMapper().Map(Split);
         System.out.println("Finished Mapping "+ hashMap.size());
         HashMap<String, Integer> indexes = Shuffler.Shuffle(mapReduce, hashMap);
         System.out.println("index "+ indexes.size());
         //System.out.println(indexes.values().stream().filter(v -> v == 0).count());
         HashMap<String,ArrayList<Integer>>[] shuffledHashMaps = Shuffler.ShuffleArrays(indexes,hashMap,mapReduce.getNumberOfReducers());
+        System.out.println(shuffledHashMaps.length);
         System.out.println(shuffledHashMaps[0]);
+        System.out.println(shuffledHashMaps[1]);
         SplittingServer splittingServer = new SplittingServer(mapReduce,shuffledHashMaps,mapReduce.getNumberOfReducers());
         splittingServer.Connect();
         System.out.println("Finished Shuffling");
