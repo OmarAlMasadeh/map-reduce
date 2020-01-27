035125759138098c28f5381da29de7e1f04d33d0
Omar alMasadeh <omarjmasadeh@gmail.com>
2020-01-27 21:37:18 +0200

    Adding files

--- src/main/java/ReducerMain.java
+++ src/main/java/ReducerMain.java
@@ -7,16 +7,16 @@ import java.util.HashMap;
 
 public class ReducerMain {
     public static void main(String[] args) {
-
         System.out.println("Reducer Created");
         SplitReceiver splitReceiver = new SplitReceiver("172.18.1.0");
         MapReduce mapReduce = splitReceiver.receiveMapReduce();
         System.out.println("mapreduce "+mapReduce.getNumberOfMappers());
         HashMap<String, ArrayList<Integer>>[] beforeReduce = new HashMap[mapReduce.getNumberOfMappers()];
         beforeReduce[0]=splitReceiver.receiveSplit();
-        System.out.println(beforeReduce[0]);
+        System.out.println(beforeReduce[0].size());
         for(int i =1;i<mapReduce.getNumberOfMappers();i++) {
-            beforeReduce[i]= new SplitReceiver("172.18.1."+i).receiveSplit();
+            splitReceiver = new SplitReceiver("172.18.1."+i);
+            beforeReduce[i]= splitReceiver.receiveSplit();
             System.out.println(beforeReduce[i].size());
         }
         while (true);
