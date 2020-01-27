cbacbddcfe0cc5200001d4ab75b293238639ae71
Omar alMasadeh <omarjmasadeh@gmail.com>
2020-01-27 23:43:54 +0200

    Adding files

--- src/main/java/ReducerMain.java
+++ src/main/java/ReducerMain.java
@@ -4,6 +4,8 @@ import org.w3c.dom.ls.LSOutput;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class ReducerMain {
     public static void main(String[] args) {
@@ -20,6 +22,22 @@ public class ReducerMain {
             beforeReduce[i]= splitReceiver.receiveSplit();
             System.out.println(beforeReduce[i].size());
         }
-        while (true);
+        HashMap<String, ArrayList<Integer>> mergedHasMap = MergeHashMaps(beforeReduce);
+        System.out.println(mergedHasMap.size());
+    }
+    private static HashMap<String, ArrayList<Integer>> MergeHashMaps(HashMap<String, ArrayList<Integer>>[] splitHashMaps){
+        HashMap<String, ArrayList<Integer>> mergedHashMap = splitHashMaps[0];
+        for(int i = 1;i<splitHashMaps.length;i++){
+            Iterator it = splitHashMaps[i].entrySet().iterator();
+            while (it.hasNext()) {
+                Map.Entry pair = (Map.Entry) it.next();
+                if(mergedHashMap.containsKey(pair.getKey())){
+                    mergedHashMap.get(pair.getKey()).addAll((ArrayList<Integer>) pair.getKey());
+                }else{
+                    mergedHashMap.put((String)pair.getKey(),(ArrayList<Integer>) pair.getValue());
+                }
+            }
+        }
+        return mergedHashMap;
     }
 }
