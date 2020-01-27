9b40a2337bd13ceca967141c3836d12cfff29aa3
Omar alMasadeh <omarjmasadeh@gmail.com>
2020-01-27 22:37:43 +0200

    Adding files

--- src/main/java/Shuffler.java
+++ src/main/java/Shuffler.java
@@ -17,14 +17,13 @@ public class Shuffler {
         HashMap[] shuffledHashMaps = new HashMap[numberOfReducers];
         for(int i = 0; i<numberOfReducers;i++) {
             shuffledHashMaps[i]=new HashMap<String,ArrayList<Integer>>();
-            System.out.println(shuffledHashMaps[i]);
         }
         Iterator it = hashMap.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry) it.next();
+            System.out.println("Key = " + pair.getKey() + ", Value = " +pair.getValue());
             int index = indexes.get(pair.getKey());
             shuffledHashMaps[index].put(pair.getKey(),pair.getValue());
-            //it.remove();
         }
         System.out.println(shuffledHashMaps[0]);
         return shuffledHashMaps;
