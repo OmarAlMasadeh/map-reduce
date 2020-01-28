96dfc35122e6aed94ab92ae8019e4451a0f348c5
Omar alMasadeh <omarjmasadeh@gmail.com>
2020-01-28 11:32:52 +0200

    Adding files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         WordCount wordCount = new WordCount();
         MapReduce mapReduce = new MapReduce.MapReduceBuilder()
-                .numberOfMappers(4)
-                .numberOfReducers(5)
+                .numberOfMappers(3)
+                .numberOfReducers(4)
                 .mapper(wordCount)
                 .reducer(wordCount)
                 .build();
