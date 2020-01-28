a48109ef882d377dacceb63335388123e2adfdf6
Omar alMasadeh <omarjmasadeh@gmail.com>
2020-01-28 12:14:28 +0200

    Adding files

--- src/main/java/ClientReceiver.java
+++ src/main/java/ClientReceiver.java
@@ -15,9 +15,10 @@ public class ClientReceiver {
         this.HOSTNAME = hostname;
         try {
             while (!(InetAddress.getByName(hostname).isReachable(36000))){}
+            Thread.sleep(2000);
             socket = new Socket(HOSTNAME,PORT);
             System.out.println("Connected to server");
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e ) {
             e.printStackTrace();
         }
     }
